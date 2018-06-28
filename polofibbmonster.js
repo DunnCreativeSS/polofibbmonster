7ec136deaedb899ea3558c45331107b7c5d4c48e
DunnCreativeSS <jarett+github@dcssquared.com>
2018-06-28 00:20:53 -0300

    remove consolelogs

--- polofibbmonster.js
+++ polofibbmonster.js
@@ -619,8 +619,7 @@ poloniex.on('message', (channelName, data, seq) => {
 			 //console.log(Object.keys(data[0].data.asks)[0]);
 			 //console.log(Object.keys(data[0].data.bids)[0]);
 			 bestAsk[channelName] = Object.keys(data[0].data.asks)[0];
-		console.log(channelName);
-			 console.log(bestAsk[channelName]);
+		
 			 update(Object.keys(data[0].data.asks)[0], Object.keys(data[0].data.bids)[0], channelName, collection)
 		 //poloniex.unsubscribe(channelName);
 		 }
@@ -628,16 +627,14 @@ poloniex.on('message', (channelName, data, seq) => {
 		 if (data[d].type =='orderBookModify'){
 			 if (data[d].data.rate <= bestAsk[channelName] && data[d].data.type == 'ask'){
 				 bestAsk[channelName] = data[d].data.rate;
-		console.log(channelName);
-			 console.log(bestAsk[channelName]);
+		
 				 update(data[d].data.rate, data[d].data.rate, channelName, collection)
 			 }
 		 }
 		 else if (data[d].type =='orderBookRemove'){
 			 if (data[d].data.rate <= bestAsk[channelName] && data[d].data.type == 'ask'){
 				 bestAsk[channelName] = data[d].data.rate;
-		console.log(channelName);
-			 console.log(bestAsk[channelName]);
+		
 				 update(999999999999999, data[d].data.rate, channelName, collection)
 			 }
 		 }
