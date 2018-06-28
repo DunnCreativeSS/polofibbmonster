e4b93fcb67c72fc972529f4e9c907ada27de4aa4
DunnCreativeSS <jarett+github@dcssquared.com>
2018-06-28 16:33:44 -0300

    multi

--- polofibbmonster.js
+++ polofibbmonster.js
@@ -106,7 +106,7 @@ app.get('/', function(req, res) {
 										data[d][a].currentBid = bestBid[data[d][a].pair];
 										data[d][a].percent = (parseFloat(data[d][a].currentBid) / parseFloat(data[d][a].rate));
 										orders.push(data[d][a]);
-										btcbal += (parseFloat(data[d][a].amount) * parseFloat(bestAsk[data[d][a].pair]))
+										btcbal += (parseFloat(data[d][a].amount) * parseFloat(bestBid[data[d][a].pair]))
 									}
 								}
 							}
@@ -146,7 +146,7 @@ app.get('/', function(req, res) {
 		console.log((totals).toString());
 		res.send('<head><link rel="icon" href="https://polofibbmonster.herokuapp.com/favicon.ico?v=2" /><meta http-equiv="refresh" content="36"><script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script></head><h1>Don\'t Panic! If the data seems off, wait a minute or so.</h1>'
 		+ 'current time: ' + new Date()
-		+ '<br>BTC Balance (including open orders if sold at current ask): ' + btcbal + '<br>'
+		+ '<br>BTC Balance (including open orders if sold at current bid): ' + btcbal + '<br>'
 		+ 'minutes: ' + minutes + '<br>'
 		+ 'hours: ' + hours + '<br>'
 		+ 'percent: ' + percent + '%<br>'
