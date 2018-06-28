e2feac2be64cc1f52bc64090a0ed626e0abc053d
DunnCreativeSS <jarett+github@dcssquared.com>
2018-06-28 16:19:16 -0300

    multi

--- polofibbmonster.js
+++ polofibbmonster.js
@@ -582,14 +582,15 @@ function collectionDo(collection, data, balances, btc){
 							//console.log('ds: ');
 							//console.log(ds);
 							if (doc3[d].trades.currencyPair){
-								if (doc3[d].trades.bought1 == true){
+								
+								if (doc3[d].trades.bought1 == true && !ds.includes(doc3[d].trades.currencyPair)){
+									if (doc3[d].trades.bought1 == true){
 									console.log('bought1 true');
 								}
 								if (!ds.includes(doc3[d].trades.currencyPair)){
 									console.log('ds no include ' + doc3[d].trades.currencyPair);
 									console.log(ds);
 								}
-								if (doc3[d].trades.bought1 == true && !ds.includes(doc3[d].trades.currencyPair)){
 									doc3[d].trades.bought1 = false;
 									doc3[d].trades.bought2 = false;
 									collection.update({
